a0b7c4326f63630afa7c9272f4789eb7f1da0afe
Judicael RANDRIAMANJATO <judi.randriamanjato@gmail.com>
2022-06-07 00:15:00 +0300

    feat: Adding insert file

---
+++ insert.sql
@@ -0,0 +1,163 @@
+--insert ville
+insert into ville (nom_ville) values ('Antananarivo');
+insert into ville (nom_ville) values ('Antsirabe');
+insert into ville (nom_ville) values ('Ambatolampy');
+insert into ville (nom_ville) values ('Fianarantsoa');
+insert into ville (nom_ville) values ('Ambositra');
+insert into ville (nom_ville) values ('Toliara');
+insert into ville (nom_ville) values ('Manakara');
+insert into ville (nom_ville) values ('Mananjary');
+insert into ville (nom_ville) values ('Tôlanaro');
+insert into ville (nom_ville) values ('Toamasina');
+insert into ville (nom_ville) values ('Ambatondrazaka');
+insert into ville (nom_ville) values ('Diego-Suarez');
+insert into ville (nom_ville) values ('Mahajanga');
+insert into ville (nom_ville) values ('Morondava');
+insert into ville (nom_ville) values ('Miandrivazo');
+insert into ville (nom_ville) values ('Maevatanana');
+insert into ville (nom_ville) values ('Ihosy');
+insert into ville (nom_ville) values ('Brickaville');
+
+--client
+insert into client (nom_client, prenom_client, contact) values ('Tiller', 'Hildy', '034 95 169 97');
+insert into client (nom_client, prenom_client, contact) values ('Gogan', 'Clayborne', '034 98 727 28');
+insert into client (nom_client, prenom_client, contact) values ('Heakey', 'Shem', '034 29 051 78');
+insert into client (nom_client, prenom_client, contact) values ('Gonnelly', 'Juliann', '034 87 475 05');
+insert into client (nom_client, prenom_client, contact) values ('Busch', 'Angie', '034 18 595 85');
+insert into client (nom_client, prenom_client, contact) values ('Nairn', 'Justen', '034 83 901 79');
+insert into client (nom_client, prenom_client, contact) values ('Vennard', 'Zorana', '034 55 668 50');
+insert into client (nom_client, prenom_client, contact) values ('Lammers', 'Lazar', '034 52 762 25');
+insert into client (nom_client, prenom_client, contact) values ('de Verson', 'Augusta', '034 62 940 80');
+insert into client (nom_client, prenom_client, contact) values ('Randlesome', 'Cris', '034 44 891 54');
+insert into client (nom_client, prenom_client, contact) values ('Witheford', 'Geri', '034 66 407 40');
+insert into client (nom_client, prenom_client, contact) values ('Jimenez', 'Halie', '034 46 362 53');
+insert into client (nom_client, prenom_client, contact) values ('Mackiewicz', 'Bianka', '034 35 743 59');
+insert into client (nom_client, prenom_client, contact) values ('Mingaud', 'Gerick', '034 20 262 31');
+insert into client (nom_client, prenom_client, contact) values ('Seabourne', 'Spencer', '034 85 824 96');
+insert into client (nom_client, prenom_client, contact) values ('Van Zon', 'Kile', '034 81 973 13');
+insert into client (nom_client, prenom_client, contact) values ('Prendeville', 'Townie', '034 61 767 86');
+insert into client (nom_client, prenom_client, contact) values ('Larby', 'Bobbie', '034 83 789 09');
+insert into client (nom_client, prenom_client, contact) values ('Hulk', 'Gerrie', '034 40 365 52');
+insert into client (nom_client, prenom_client, contact) values ('Quadling', 'Maggee', '034 68 004 00');
+insert into client (nom_client, prenom_client, contact) values ('Illidge', 'Hillary', '034 99 729 44');
+insert into client (nom_client, prenom_client, contact) values ('Britner', 'Faye', '034 92 460 17');
+insert into client (nom_client, prenom_client, contact) values ('Blackey', 'Corty', '034 68 626 05');
+insert into client (nom_client, prenom_client, contact) values ('Thatcham', 'Emory', '034 00 548 85');
+insert into client (nom_client, prenom_client, contact) values ('Giddons', 'Manuel', '034 73 386 30');
+insert into client (nom_client, prenom_client, contact) values ('Battson', 'Minerva', '034 11 725 74');
+insert into client (nom_client, prenom_client, contact) values ('Lacelett', 'Delia', '034 01 313 19');
+insert into client (nom_client, prenom_client, contact) values ('Rosoni', 'Wyatan', '034 13 248 64');
+insert into client (nom_client, prenom_client, contact) values ('Macenzy', 'Meredeth', '034 24 683 34');
+insert into client (nom_client, prenom_client, contact) values ('Wellfare', 'Nina', '034 75 204 35');
+insert into client (nom_client, prenom_client, contact) values ('Jaqueme', 'Maurizia', '034 03 611 41');
+
+--insert chauffeur
+insert into chauffeur (nom_complet, contact_chauffeur, mail_chauffeur, id_vehicule) values ('Hamilton Caccavari', '034 70 863 63', 'hcaccavaris@reference.com','1');
+insert into chauffeur (nom_complet, contact_chauffeur, mail_chauffeur, id_vehicule) values ('Jeffy Devenish', '034 93 367 25', 'jdevenisht@bandcamp.com','2');
+insert into chauffeur (nom_complet, contact_chauffeur, mail_chauffeur, id_vehicule) values ('Ric Trees', '034 22 747 80', 'rtreesu@amazon.de','3');
+insert into chauffeur (nom_complet, contact_chauffeur, mail_chauffeur, id_vehicule) values ('Roana Killshaw', '034 61 134 30', 'rkillshawv@statcounter.com','4');
+insert into chauffeur (nom_complet, contact_chauffeur, mail_chauffeur, id_vehicule) values ('Giacopo Forber', '034 49 221 31', 'gforberw@xinhuanet.com','5');
+insert into chauffeur (nom_complet, contact_chauffeur, mail_chauffeur, id_vehicule) values ('Clarine Veal', '034 06 473 34', 'cvealx@jigsy.com','6');
+insert into chauffeur (nom_complet, contact_chauffeur, mail_chauffeur, id_vehicule) values ('Issie O''Duane', '034 46 463 69', 'ioduaney@examiner.com','7');
+insert into chauffeur (nom_complet, contact_chauffeur, mail_chauffeur, id_vehicule) values ('Dareen Shave', '034 39 530 67', 'dshavez@spiegel.de','8');
+insert into chauffeur (nom_complet, contact_chauffeur, mail_chauffeur, id_vehicule) values ('Reena Hiom', '034 57 008 80', 'rhiom10@youku.com','9');
+insert into chauffeur (nom_complet, contact_chauffeur, mail_chauffeur, id_vehicule) values ('Phillis Kewzick', '034 59 540 33', 'pkewzick11@google.com','10');
+insert into chauffeur (nom_complet, contact_chauffeur, mail_chauffeur, id_vehicule) values ('Steward Eltone', '034 18 938 91', 'seltone12@bloglines.com','11');
+insert into chauffeur (nom_complet, contact_chauffeur, mail_chauffeur, id_vehicule) values ('Suzanne Dodswell', '034 31 881 13', 'sdodswell13@artisteer.com','12');
+insert into chauffeur (nom_complet, contact_chauffeur, mail_chauffeur, id_vehicule) values ('Krystle Gullam', '034 27 473 76', 'kgullam14@ucla.edu','13');
+insert into chauffeur (nom_complet, contact_chauffeur, mail_chauffeur, id_vehicule) values ('Mariam Pryde', '034 16 245 52', 'mpryde15@tamu.edu','14');
+insert into chauffeur (nom_complet, contact_chauffeur, mail_chauffeur, id_vehicule) values ('Jae Menat', '034 03 657 70', 'jmenat16@weather.com','15');
+insert into chauffeur (nom_complet, contact_chauffeur, mail_chauffeur, id_vehicule) values ('Isis Pinkney', '034 25 503 25', 'ipinkney17@squidoo.com','16');
+insert into chauffeur (nom_complet, contact_chauffeur, mail_chauffeur, id_vehicule) values ('Mari Makin', '034 93 321 14', 'mmakin18@symantec.com','17');
+insert into chauffeur (nom_complet, contact_chauffeur, mail_chauffeur, id_vehicule) values ('Jodee Moens', '034 53 891 50', 'jmoensh@alibaba.com','18');
+insert into chauffeur (nom_complet, contact_chauffeur, mail_chauffeur, id_vehicule) values ('Oralie Mulvy', '034 30 387 43', 'omulvyi@msn.com','19');
+insert into chauffeur (nom_complet, contact_chauffeur, mail_chauffeur, id_vehicule) values ('Lucilia Collinge', '034 88 007 28', 'lcollingej@issuu.com','20');
+
+-- offre
+insert into offre (type,tarif) values ('lite','10000');
+insert into offre (type,tarif) values ('premium','20000');
+insert into offre (type,tarif) values ('vip','30000');
+
+-- insert vehicules
+insert into vehicules (nombre_places, places_dispo, matricule) values (22, 6, '9208TBN');
+insert into vehicules (nombre_places, places_dispo, matricule) values (22, 2, '8663TBG');
+insert into vehicules (nombre_places, places_dispo, matricule) values (22, 0, '5412TAA');
+insert into vehicules (nombre_places, places_dispo, matricule) values (22, 8, '7653TBJ');
+insert into vehicules (nombre_places, places_dispo, matricule) values (22, 0, '3562TBD');
+insert into vehicules (nombre_places, places_dispo, matricule) values (22, 0, '9896TAK');
+insert into vehicules (nombre_places, places_dispo, matricule) values (22, 7, '7835TBL');
+insert into vehicules (nombre_places, places_dispo, matricule) values (22, 1, '4725TAC');
+insert into vehicules (nombre_places, places_dispo, matricule) values (22, 9, '3608TBB');
+insert into vehicules (nombre_places, places_dispo, matricule) values (22, 10,'4987TAV');
+
+--utiliser
+insert into utiliser (id_vehicule, id_voyage) values ('1', '1');
+insert into utiliser (id_vehicule, id_voyage) values ('2', '2');
+insert into utiliser (id_vehicule, id_voyage) values ('3', '3');
+insert into utiliser (id_vehicule, id_voyage) values ('4', '4');
+insert into utiliser (id_vehicule, id_voyage) values ('5', '5');
+insert into utiliser (id_vehicule, id_voyage) values ('6', '6');
+insert into utiliser (id_vehicule, id_voyage) values ('7', '7');
+insert into utiliser (id_vehicule, id_voyage) values ('8', '8');
+insert into utiliser (id_vehicule, id_voyage) values ('9', '9');
+insert into utiliser (id_vehicule, id_voyage) values ('10', '10');
+
+-- reserver
+insert into reserver (id_client, id_offre, id_voyage, place, montant_paye) values (1, 1, 31, 7, 0);
+insert into reserver (id_client, id_offre, id_voyage, place, montant_paye) values (2, 2, 18, 10, 20000);
+insert into reserver (id_client, id_offre, id_voyage, place, montant_paye) values (3, 1, 3, 9, 10000);
+insert into reserver (id_client, id_offre, id_voyage, place, montant_paye) values (4, 3, 16, 2, 30000);
+insert into reserver (id_client, id_offre, id_voyage, place, montant_paye) values (5, 3, 8, 7, 30000);
+insert into reserver (id_client, id_offre, id_voyage, place, montant_paye) values (6, 2, 20, 5, 20000);
+insert into reserver (id_client, id_offre, id_voyage, place, montant_paye) values (7, 3, 35, 4, 0);
+insert into reserver (id_client, id_offre, id_voyage, place, montant_paye) values (8, 1, 4, 1, 10000);
+insert into reserver (id_client, id_offre, id_voyage, place, montant_paye) values (9, 1, 27, 1, 10000);
+insert into reserver (id_client, id_offre, id_voyage, place, montant_paye) values (10, 2, 21, 1, 2000);
+insert into reserver (id_client, id_offre, id_voyage, place, montant_paye) values (11, 2, 1, 10, 0);
+insert into reserver (id_client, id_offre, id_voyage, place, montant_paye) values (12, 1, 20, 5, 10000);
+insert into reserver (id_client, id_offre, id_voyage, place, montant_paye) values (13, 3, 16, 1, 30000);
+insert into reserver (id_client, id_offre, id_voyage, place, montant_paye) values (14, 1, 30, 3, 10000);
+insert into reserver (id_client, id_offre, id_voyage, place, montant_paye) values (15, 1, 28, 10, 10000);
+insert into reserver (id_client, id_offre, id_voyage, place, montant_paye) values (16, 1, 15, 1, 10000);
+insert into reserver (id_client, id_offre, id_voyage, place, montant_paye) values (17, 3, 25, 6, 0);
+insert into reserver (id_client, id_offre, id_voyage, place, montant_paye) values (18, 1, 18, 10, 10000);
+insert into reserver (id_client, id_offre, id_voyage, place, montant_paye) values (19, 1, 23, 5, 10000);
+insert into reserver (id_client, id_offre, id_voyage, place, montant_paye) values (20, 1, 2, 3, 1000);
+insert into reserver (id_client, id_offre, id_voyage, place, montant_paye) values (21, 3, 12, 9, 30000);
+insert into reserver (id_client, id_offre, id_voyage, place, montant_paye) values (22, 3, 31, 4, 0);
+insert into reserver (id_client, id_offre, id_voyage, place, montant_paye) values (23, 1, 10, 4, 10000);
+
+--voyage
+insert into voyage (jour_depart, heure_depart, id_ville_depart, id_ville_arrivee) values ('2022/06/09', '11:00', '1', '11');
+insert into voyage (jour_depart, heure_depart, id_ville_depart, id_ville_arrivee) values ('2022/06/09', '13:00', '2', '1');
+insert into voyage (jour_depart, heure_depart, id_ville_depart, id_ville_arrivee) values ('2022/06/09', '15:00', '5', '2');
+insert into voyage (jour_depart, heure_depart, id_ville_depart, id_ville_arrivee) values ('2022/06/09', '19:00', '7', '3');
+insert into voyage (jour_depart, heure_depart, id_ville_depart, id_ville_arrivee) values ('2022/06/09', '21:00', '3', '4');
+insert into voyage (jour_depart, heure_depart, id_ville_depart, id_ville_arrivee) values ('2022/06/09', '23:00', '2', '15');
+insert into voyage (jour_depart, heure_depart, id_ville_depart, id_ville_arrivee) values ('2022/06/10', '09:00', '1', '6');
+insert into voyage (jour_depart, heure_depart, id_ville_depart, id_ville_arrivee) values ('2022/06/10', '11:00', '2', '7');
+insert into voyage (jour_depart, heure_depart, id_ville_depart, id_ville_arrivee) values ('2022/06/10', '13:00', '9', '8');
+insert into voyage (jour_depart, heure_depart, id_ville_depart, id_ville_arrivee) values ('2022/06/10', '15:00', '2', '9');
+insert into voyage (jour_depart, heure_depart, id_ville_depart, id_ville_arrivee) values ('2022/06/10', '19:00', '3', '12');
+insert into voyage (jour_depart, heure_depart, id_ville_depart, id_ville_arrivee) values ('2022/06/10', '21:00', '10', '13');
+insert into voyage (jour_depart, heure_depart, id_ville_depart, id_ville_arrivee) values ('2022/06/10', '23:00', '3', '16');
+insert into voyage (jour_depart, heure_depart, id_ville_depart, id_ville_arrivee) values ('2022/06/12', '09:00', '4', '10');
+insert into voyage (jour_depart, heure_depart, id_ville_depart, id_ville_arrivee) values ('2022/06/12', '11:00', '9', '18');
+insert into voyage (jour_depart, heure_depart, id_ville_depart, id_ville_arrivee) values ('2022/06/12', '13:00', '5', '17');
+insert into voyage (jour_depart, heure_depart, id_ville_depart, id_ville_arrivee) values ('2022/06/12', '15:00', '8', '5');
+
+-- maintenance
+insert into maintenance (nom_maintenance,cout) values ('réparation','140000');
+insert into maintenance (nom_maintenance,cout) values ('essence','70000');
+
+-- requerir
+insert into requerir (id_maintenance, id_vehicule) values ('1', '1');
+insert into requerir (id_maintenance, id_vehicule) values ('2', '9');
+insert into requerir (id_maintenance, id_vehicule) values ('1', '7');
+insert into requerir (id_maintenance, id_vehicule) values ('2', '2');
+insert into requerir (id_maintenance, id_vehicule) values ('1', '5');
+insert into requerir (id_maintenance, id_vehicule) values ('2', '8');
+insert into requerir (id_maintenance, id_vehicule) values ('1', '1');
+insert into requerir (id_maintenance, id_vehicule) values ('2', '4');
+insert into requerir (id_maintenance, id_vehicule) values ('1', '6');
+insert into requerir (id_maintenance, id_vehicule) values ('2', '3');
