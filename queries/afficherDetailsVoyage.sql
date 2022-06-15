3cf54e8d13817b8554683d9af2f710869fc2f6a2
Mihary Joel <72041096+miharyjoe@users.noreply.github.com>
2022-06-15 22:44:22 +0300

    Update afficherDetailsVoyage.sql

--- queries/afficherDetailsVoyage.sql
+++ queries/afficherDetailsVoyage.sql
@@ -1,10 +1,6 @@
 --req 38: afficher les details d'une voyage
 
-	--jour depart, heure_depart,ville depart
-	select jour_depart, heure_depart,nom_ville from voyage 
-	inner join ville on voyage.id_ville_depart = ville.id_ville;
-	
-	--jour depart, heure_depart,ville dest
-	select jour_depart, heure_depart,nom_ville from voyage 
-	inner join ville on voyage.id_ville_arrivee = ville.id_ville;
+select jour_depart, heure_depart, v1.nom_ville as ville_depart , v2.nom_ville as ville_arrivee from voyage 
+	inner join ville as v1 on voyage.id_ville_depart = v1.id_ville
+	inner join ville as v2 on voyage.id_ville_arrivee = v2.id_ville;
 	
